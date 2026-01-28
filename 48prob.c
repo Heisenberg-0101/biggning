d63cdaedec35449e976d91e87ceca9c006c35383
Ankit Chaudhary <ac484709@gmail.com>
2026-01-28 23:44:34 +0530

    Add files via upload

---
+++ 48prob.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+       
+      int n;
+      printf("enter a number:");
+      scanf("%d",&n);
+      for(int i = n; i >= 1; i--) {
+        for(int j = n; j >= i; j--) {
+            printf("* ");
+        }
+        printf("\n");
+      }
+
+return 0;
+}
